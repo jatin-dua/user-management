54274e7dbe95fbf84e015091210a0972ea935dc5
Jatin Dua <jdua004@gmail.com>
2024-09-22 19:31:52 +0530

    feat: Add service parameters

--- src/Controller/BackupController.php
+++ src/Controller/BackupController.php
@@ -11,8 +11,15 @@ class BackupController extends AbstractController
     #[Route('/api/backup', name: 'app_backup', methods: ['GET'])]
     public function backup(): Response
     {
+        $dbName = $this->getParameter('database_name');
+        $dbUser = $this->getParameter('database_user');
+        $dbPassword = $this->getParameter('database_password');
+
         return $this->render('backup/index.html.twig', [
             'controller_name' => 'BackupController',
+            'database_name' => $dbName,
+            'database_user' => $dbUser,
+            'database_password' => $dbPassword,
         ]);
     }
 }
