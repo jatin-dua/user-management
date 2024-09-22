89031fc7569c112c954118a17d491f4fef2c3924
Jatin Dua <jdua004@gmail.com>
2024-09-22 20:14:17 +0530

    feat: Implement DB Restore endpoint

---
+++ src/Controller/RestoreController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
+
+class RestoreController extends AbstractController
+{
+    #[Route('/api/restore', name: 'app_restore', methods: ['POST'])]
+    public function index(): Response
+    {
+        $dbName = $this->getParameter('database_name');
+        $dbUser = $this->getParameter('database_user');
+        $dbPassword = $this->getParameter('database_password');
+        
+        $projectDir = $this->getParameter('kernel.project_dir');
+        $backupDir = "$projectDir/db_backup";
+        $backupFile = 'backup.sql';
+        $backupPath = "$backupDir/$backupFile";
+
+        $backupCommand = [
+            'docker',
+            'exec',
+            '-i',
+            'database',
+            'mysql',
+            '-u',
+            $dbUser,
+            "-p$dbPassword",
+            $dbName,
+        ];
+
+        $process = new Process($backupCommand);
+        $process->setTimeout(300);
+        $process->setInput(file_get_contents($backupPath));
+        // Execute the command and capture output
+        try {
+            $process->mustRun();
+            return new Response("Database restored from $backupPath", Response::HTTP_OK);
+        } catch (ProcessFailedException $exception) {
+            // Log the exception details
+            return new Response(
+                'Failed to restore database: ' . $exception->getMessage(),
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
+    }
+}
