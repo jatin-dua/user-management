053ac1c852438a0cb4a60fc3e97e813f53d76eef
Jatin Dua <jdua004@gmail.com>
2024-09-22 19:52:47 +0530

    feat: Implement DB Backup endpoint

--- src/Controller/BackupController.php
+++ src/Controller/BackupController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
 
 class BackupController extends AbstractController
 {
@@ -14,12 +16,36 @@ class BackupController extends AbstractController
         $dbName = $this->getParameter('database_name');
         $dbUser = $this->getParameter('database_user');
         $dbPassword = $this->getParameter('database_password');
+        
+        $projectDir = $this->getParameter('kernel.project_dir');
+        $backupDir = "$projectDir/db_backup";
+        $backupFile = 'backup.sql';
+        $backupPath = "$backupDir/$backupFile";
 
-        return $this->render('backup/index.html.twig', [
-            'controller_name' => 'BackupController',
-            'database_name' => $dbName,
-            'database_user' => $dbUser,
-            'database_password' => $dbPassword,
-        ]);
+        $backupCommand = [
+            'docker',
+            'exec',
+            'database',
+            'mysqldump',
+            '-u',
+            $dbUser,
+            "-p$dbPassword",
+            $dbName,
+        ];
+
+        $process = new Process($backupCommand);
+        $process->setTimeout(300);
+        
+        try {
+            $process->mustRun();
+            $output = $process->getOutput();
+            file_put_contents($backupPath, $output);
+            return new Response("Database backup created at Path: $backupPath", Response::HTTP_OK);
+        } catch (ProcessFailedException $exception) {
+            return new Response(
+                'Failed to create database backup: ' . $exception->getMessage(),
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
     }
 }
