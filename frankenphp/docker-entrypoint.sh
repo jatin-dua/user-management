661230dc49857948768bb4503b0e85fdf3649fa5
Jatin Dua <jdua004@gmail.com>
2024-10-09 14:20:42 +0530

    feat: Auto generate JWT keys on startup

--- frankenphp/docker-entrypoint.sh
+++ frankenphp/docker-entrypoint.sh
@@ -2,59 +2,70 @@
 set -e
 
 if [ "$1" = 'frankenphp' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
-	# Install the project the first time PHP is started
-	# After the installation, the following block can be deleted
-	if [ ! -f composer.json ]; then
-		rm -Rf tmp/
-		composer create-project "symfony/skeleton $SYMFONY_VERSION" tmp --stability="$STABILITY" --prefer-dist --no-progress --no-interaction --no-install
+    # Install the project the first time PHP is started
+    # After the installation, the following block can be deleted
+    if [ ! -f composer.json ]; then
+        rm -Rf tmp/
+        composer create-project "symfony/skeleton $SYMFONY_VERSION" tmp --stability="$STABILITY" --prefer-dist --no-progress --no-interaction --no-install
 
-		cd tmp
-		cp -Rp . ..
-		cd -
-		rm -Rf tmp/
+        cd tmp
+        cp -Rp . ..
+        cd -
+        rm -Rf tmp/
 
-		composer require "php:>=$PHP_VERSION" runtime/frankenphp-symfony
-		composer config --json extra.symfony.docker 'true'
+        composer require "php:>=$PHP_VERSION" runtime/frankenphp-symfony
+        composer config --json extra.symfony.docker 'true'
 
-		if grep -q ^DATABASE_URL= .env; then
-			echo "To finish the installation please press Ctrl+C to stop Docker Compose and run: docker compose up --build -d --wait"
-			sleep infinity
-		fi
-	fi
+        if grep -q ^DATABASE_URL= .env; then
+            echo "To finish the installation please press Ctrl+C to stop Docker Compose and run: docker compose up --build -d --wait"
+            sleep infinity
+        fi
+    fi
 
-	if [ -z "$(ls -A 'vendor/' 2>/dev/null)" ]; then
-		composer install --prefer-dist --no-progress --no-interaction
-	fi
+    if [ -z "$(ls -A 'vendor/' 2>/dev/null)" ]; then
+        composer install --prefer-dist --no-progress --no-interaction
+    fi
 
-	if grep -q ^DATABASE_URL= .env; then
-		echo "Waiting for database to be ready..."
-		ATTEMPTS_LEFT_TO_REACH_DATABASE=60
-		until [ $ATTEMPTS_LEFT_TO_REACH_DATABASE -eq 0 ] || DATABASE_ERROR=$(php bin/console dbal:run-sql -q "SELECT 1" 2>&1); do
-			if [ $? -eq 255 ]; then
-				# If the Doctrine command exits with 255, an unrecoverable error occurred
-				ATTEMPTS_LEFT_TO_REACH_DATABASE=0
-				break
-			fi
-			sleep 1
-			ATTEMPTS_LEFT_TO_REACH_DATABASE=$((ATTEMPTS_LEFT_TO_REACH_DATABASE - 1))
-			echo "Still waiting for database to be ready... Or maybe the database is not reachable. $ATTEMPTS_LEFT_TO_REACH_DATABASE attempts left."
-		done
+    # Path to the JWT keys (inside the container)
+    JWT_PRIVATE_KEY_PATH=config/jwt/private.pem
+    JWT_PUBLIC_KEY_PATH=config/jwt/public.pem # Check if the keys do not exist and generate them
 
-		if [ $ATTEMPTS_LEFT_TO_REACH_DATABASE -eq 0 ]; then
-			echo "The database is not up or not reachable:"
-			echo "$DATABASE_ERROR"
-			exit 1
-		else
-			echo "The database is now ready and reachable"
-		fi
+    if [ ! -f "$JWT_PRIVATE_KEY_PATH" ] || [ ! -f "$JWT_PUBLIC_KEY_PATH" ]; then
+        echo "Generating JWT keys..."
+        php bin/console lexik:jwt:generate-keypair --overwrite --no-interaction
+    else
+        echo "JWT keys already exist."
+    fi
 
-		if [ "$( find ./migrations -iname '*.php' -print -quit )" ]; then
-			php bin/console doctrine:migrations:migrate --no-interaction --all-or-nothing
-		fi
-	fi
+    if grep -q ^DATABASE_URL= .env; then
+        echo "Waiting for database to be ready..."
+        ATTEMPTS_LEFT_TO_REACH_DATABASE=60
+        until [ $ATTEMPTS_LEFT_TO_REACH_DATABASE -eq 0 ] || DATABASE_ERROR=$(php bin/console dbal:run-sql -q "SELECT 1" 2>&1); do
+            if [ $? -eq 255 ]; then
+                # If the Doctrine command exits with 255, an unrecoverable error occurred
+                ATTEMPTS_LEFT_TO_REACH_DATABASE=0
+                break
+            fi
+            sleep 1
+            ATTEMPTS_LEFT_TO_REACH_DATABASE=$((ATTEMPTS_LEFT_TO_REACH_DATABASE - 1))
+            echo "Still waiting for database to be ready... Or maybe the database is not reachable. $ATTEMPTS_LEFT_TO_REACH_DATABASE attempts left."
+        done
 
-	setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX var
-	setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX var
+        if [ $ATTEMPTS_LEFT_TO_REACH_DATABASE -eq 0 ]; then
+            echo "The database is not up or not reachable:"
+            echo "$DATABASE_ERROR"
+            exit 1
+        else
+            echo "The database is now ready and reachable"
+        fi
+
+        if [ "$(find ./migrations -iname '*.php' -print -quit)" ]; then
+            php bin/console doctrine:migrations:migrate --no-interaction --all-or-nothing
+        fi
+    fi
+
+    setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX var
+    setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX var
 fi
 
 exec docker-php-entrypoint "$@"
